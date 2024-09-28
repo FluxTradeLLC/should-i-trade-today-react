f62201e630aaf57acd4c6cb6e77e9c16620a0599
Grant Reighard <grant.reighard@gmail.com>
2024-09-27 23:12:41 -0400

    Gets custom style working

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const localizer = momentLocalizer(moment);
 const myEventsList = [
   {
     id: 1,
-    title: "62.5",
+    title: 62.5,
     start: new Date(2024, 8, 27),
     end: new Date(2024, 8, 27),
   },
@@ -23,11 +23,31 @@ const ColoredDateCellWrapper = ({ children }) =>
     },
   });
 
+const CustomEvent = ({ event }) => {
+  const eventStyle = {
+    backgroundColor: event.title > 60 ? "green" : "red",
+    color: "white",
+    borderRadius: "5px",
+    padding: "2px 5px",
+  };
+
+  return <div style={eventStyle}>{event.title}</div>;
+};
+
+const customSlotPropGetter = (date) => {
+  if (date.getDate() === 7 || date.getDate() === 15)
+    return {
+      // className: styles.specialDay,
+    };
+  else return {};
+};
+
 function App() {
   const { components, defaultDate, max, views } = useMemo(
     () => ({
       components: {
         timeSlotWrapper: ColoredDateCellWrapper,
+        event: CustomEvent,
       },
       defaultDate: new Date(2015, 3, 1),
       max: dates.add(dates.endOf(new Date(2015, 17, 1), "day"), -1, "hours"),
@@ -47,6 +67,8 @@ function App() {
         showMultiDayTimes
         step={60}
         views={views}
+        defaultView={Views.AGENDA}
+        slotPropGetter={customSlotPropGetter}
         style={{ height: 500, width: "50%" }}
       />
     </div>
