ceacf89cd9642c4ffd79c01de5a387a253eb4a0b
Grant Reighard <grant.reighard@gmail.com>
2026-03-02 19:16:35 -0500

    do 90 days of recommendations

--- src/App.js
+++ src/App.js
@@ -116,7 +116,7 @@ function App() {
 
   useEffect(() => {
     fetch(
-      `${process.env.REACT_APP_SERVER_BASE_URI}/api/shoulditrade/get_recommendations/past_month`
+      `${process.env.REACT_APP_SERVER_BASE_URI}/api/shoulditrade/get_recommendations/past_months`
     )
       .then((response) => response.json())
       .then((data) => {
